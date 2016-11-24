246fcacf39ebf67a5b2873466444056c1f35c94f
pandemicwar <VasilevVS007@outlook.com>
2016-11-24 12:35:26 +0300

    Update createtable.php

--- createtable.php
+++ createtable.php
@@ -3,7 +3,7 @@
 $host = "localhost\sqlexpress";
 $user = "vasilevvs007";
 $pwd = "Spacedementia9";
-$db = "registration";
+$db = "mysqlbase";
 try {
     $conn = new PDO("sqlsrv:server = tcp:sqlbaseofrthesite.database.windows.net,1433; Database = mysqlbase", "vasilevvs007", "Spacedementia9");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
