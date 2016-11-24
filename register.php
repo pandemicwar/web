a9c2b312d09d192965bc6b423eb4bfb66269f23a
pandemicwar <VasilevVS007@outlook.com>
2016-11-24 13:23:57 +0300

    Update register.php

--- register.php
+++ register.php
@@ -1,8 +1,58 @@
 <?
 // Страница регистрации нового пользователя
-
+$host = "localhost\sqlexpress";
+$user = "vasilevvs007";
+$pwd = "Spacedementia9";
+$db = "mysqlbase";
 # Соединямся с БД
-$link=mysqli_connect("localhost/sqlexpress", "mysqlbase", "vasilevvs007", "Spacedementia9");
+try {
+    $conn = new PDO("sqlsrv:server = tcp:sqlbaseofrthesite.database.windows.net,1433; Database = mysqlbase", "vasilevvs007", "Spacedementia9");
+    $conn->setAttribute
+( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+}
+catch (PDOException $e) {
+    print("Error connecting to SQL Server.");
+    die(print_r($e));
+}
+if(!empty($_POST)) {
+try {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $date = date("Y-m-d");
+    // Insert data
+    $sql_insert = 
+"INSERT INTO registration_tbl (name, email, date) 
+                   VALUES (?,?,?)";
+    $stmt = $conn->prepare($sql_insert);
+    $stmt->bindValue(1, $name);
+    $stmt->bindValue(2, $email);
+    $stmt->bindValue(3, $date);
+    $stmt->execute();
+}
+catch(Exception $e) {
+    die(var_dump($e));
+}
+echo "<h3>Your're registered!</h3>";
+}
+$sql_select = "SELECT * FROM registration_tbl";
+$stmt = $conn->query($sql_select);
+$registrants = $stmt->fetchAll(); 
+if(count($registrants) > 0) {
+    echo "<h2>People who are registered:</h2>";
+    echo "<table>";
+    echo "<tr><th>Name</th>";
+    echo "<th>Email</th>";
+    echo "<th>Date</th></tr>";
+    foreach($registrants as $registrant) {
+        echo "<tr><td>".$registrant['name']."</td>";
+        echo "<td>".$registrant['email']."</td>";
+        echo "<td>".$registrant['date']."</td></tr>";
+    }
+    echo "</table>";
+} else {
+    echo "<h3>No one is currently registered.</h3>";
+}
+
 
 if(isset($_POST['submit']))
 {
@@ -33,7 +83,7 @@ if(isset($_POST['submit']))
         $login = $_POST['login'];
 
         # Убераем лишние пробелы и делаем двойное шифрование
-        $password = md5(md5(trim($_POST['password'])));
+    
 
         mysqli_query($link,"INSERT INTO users SET user_login='".$login."', user_password='".$password."'");
         header("Location: login.php"); exit();
