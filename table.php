ee4d6c56c8576182bfcea29b67bb7d8adf0bfb04
pandemicwar <VasilevVS007@outlook.com>
2016-11-28 11:07:10 +0300

    Update table.php

--- table.php
+++ table.php
@@ -1,6 +1,6 @@
 <?php
 // DB connection info
-$host = "localhost\sqlexpress";
+$host = "sqlbaseofrthesite.database.windows.net,1433";
 $user = "vasilevvs007";
 $pwd = "Spacedementia9";
 $db = "mysqlbase";
