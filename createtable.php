0f256793457b7f3483103a36fde23d669336dc4d
pandemicwar <VasilevVS007@outlook.com>
2016-11-28 11:13:21 +0300

    Update and rename users.php to createtable.php

--- createtable.php
+++ createtable.php
@@ -7,7 +7,7 @@ $db = "mysqlbase";
 try {
     $conn = new PDO("sqlsrv:server = tcp:sqlbaseofrthesite.database.windows.net,1433; Database = "mysqlbase", "vasilevvs007", "Spacedementia9");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $sql = "CREATE TABLE users(
+    $sql = "CREATE TABLE create_usr(
     user_id INT NOT NULL IDENTITY(1,1) 
     user_login VARCHAR(30),
     user_password VARCHAR(30),
