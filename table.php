114bb3dd55c2926f3968b6e5547a53fc52cc4adc
pandemicwar <VasilevVS007@outlook.com>
2016-11-28 10:50:27 +0300

    Update table.php

--- table.php
+++ table.php
@@ -9,7 +9,7 @@ try {
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $sql = "CREATE TABLE users(
     user_id INT NOT NULL IDENTITY(1,1) 
-    user_login VARCHAR(30),]
+    user_login VARCHAR(30),
     user_password VARCHAR(30),
     PRIMARY KEY(user_id),
     user_email VARCHAR(30),
