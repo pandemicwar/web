16c44655a2eb4d2af6617512d39024b475d3a1c5
pandemicwar <VasilevVS007@outlook.com>
2016-11-28 10:48:15 +0300

    Create check.php

---
+++ check.php
@@ -0,0 +1,38 @@
+try {
+    $conn = new PDO("sqlsrv:server = tcp:sqlbaseofrthesite.database.windows.net,1433; Database = mysqlbase", "vasilevvs007", "Spacedementia9");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $sql = "CREATE TABLE registration_tbl1(
+    id INT NOT NULL IDENTITY(1,1) 
+    PRIMARY KEY(id),
+    user_login VARCHAR(30),
+    user_password VARCHAR(30),
+    user_email VARCHAR(30),
+    date DATE)";
+    $conn->query($sql);
+}
+catch (PDOException $e) {
+    print("Error connecting to SQL Server.");
+    die(print_r($e));
+}
+if (isset($_COOKIE['id']) and isset($_COOKIE['hash']))
+{
+    $query = mysqli_query($link, "SELECT *,INET_NTOA(user_ip) AS user_ip FROM users WHERE user_id = '".intval($_COOKIE['id'])."' LIMIT 1");
+    $userdata = mysqli_fetch_assoc($query);
+
+    if(($userdata['user_hash'] !== $_COOKIE['hash']) or ($userdata['user_id'] !== $_COOKIE['id'])
+ or (($userdata['user_ip'] !== $_SERVER['REMOTE_ADDR'])  and ($userdata['user_ip'] !== "0")))
+    {
+        setcookie("id", "", time() - 3600*24*30*12, "/");
+        setcookie("hash", "", time() - 3600*24*30*12, "/");
+        print "Хм, что-то не получилось";
+    }
+    else
+    {
+        print "Привет, ".$userdata['user_login'].". Всё работает!";
+    }
+}
+else
+{
+    print "Включите куки";
+}
+?>
