bf5cf58b7ff48c8dc4aa2a22c292ed7952b312ae
pandemicwar <VasilevVS007@outlook.com>
2016-12-01 15:48:59 +0400

    Update register.php

--- register.php
+++ register.php
@@ -8,7 +8,7 @@ $db = "mysqlbase";
 try {
     
        
-    $conn = new PDO("sqlsrv:server = tcp:sqlbaseofrthesite.database.windows.net,1433; Database = mysqlbase", "vasilevvs007", "Spacedementia9");
+    $conn = new PDO("sqlsrv:server = tcp:serverforsqlbase.database.windows.net,1433; Database = mysqlbase", "vasilevvs007", "Spacedementia9");
     $conn->setAttribute
 ( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
 }
