0f613275a88f7aa0a86ee6a40785f96a8601eb23
pandemicwar <VasilevVS007@outlook.com>
2016-12-17 00:48:01 +0300

    Update login.php

--- login.php
+++ login.php
@@ -18,19 +18,12 @@ if(isset($_POST['submit']))
         # Генерируем случайное число и шифруем его
     
 
-        if(!@$_POST['not_attach_ip'])
-        {
-            # Если пользователя выбрал привязку к IP
-            # Переводим IP в строку
-            $insip = ", user_ip=INET_ATON('".$_SERVER['REMOTE_ADDR']."')";
-        }
-
+       
         # Записываем в БД новый хеш авторизации и IP
         query($conn, "UPDATE users SET user_hash='".$hash."' ".$insip." WHERE user_id='".$data['user_id']."'");
 
         # Ставим куки
-        setcookie("id", $data['user_id'], time()+60*60*24*30);
-        setcookie("hash", $hash, time()+60*60*24*30);
+    
 
         # Переадресовываем браузер на страницу проверки нашего скрипта
         header("Location: check.php"); exit();
@@ -44,6 +37,5 @@ if(isset($_POST['submit']))
 <form method="POST">
 Логин <input name="login" type="text"><br>
 Пароль <input name="password" type="password"><br>
-Не прикреплять к IP(не безопасно) <input type="checkbox" name="not_attach_ip"><br>
 <input name="submit" type="submit" value="Войти">
 </form>
