ea98db36ae1931ea7901cd5b48eb881db0bab36f
pandemicwar <VasilevVS007@outlook.com>
2016-12-17 00:35:46 +0300

    Update check.php

--- check.php
+++ check.php
@@ -1,13 +1,7 @@
 try {
-    $conn = new PDO("sqlsrv:server = tcp:sqlbaseofrthesite.database.windows.net,1433; Database = mysqlbase", "vasilevvs007", "Spacedementia9");
+    $conn = new PDO("sqlsrv:server = tcp:serverforsqlbase.database.windows.net,1433; Database = mysqlbase", "vasilevvs007", "Spacedementia9");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $sql = "CREATE TABLE registration_tbl1(
-    id INT NOT NULL IDENTITY(1,1) 
-    PRIMARY KEY(id),
-    user_login VARCHAR(30),
-    user_password VARCHAR(30),
-    user_email VARCHAR(30),
-    date DATE)";
+    
     $conn->query($sql);
 }
 catch (PDOException $e) {
@@ -16,17 +10,10 @@ catch (PDOException $e) {
 }
 if (isset($_COOKIE['id']) and isset($_COOKIE['hash']))
 {
-    $query = mysqli_query($link, "SELECT *,INET_NTOA(user_ip) AS user_ip FROM users WHERE user_id = '".intval($_COOKIE['id'])."' LIMIT 1");
+    $query = query($conn, "SELECT *);
     $userdata = mysqli_fetch_assoc($query);
 
-    if(($userdata['user_hash'] !== $_COOKIE['hash']) or ($userdata['user_id'] !== $_COOKIE['id'])
- or (($userdata['user_ip'] !== $_SERVER['REMOTE_ADDR'])  and ($userdata['user_ip'] !== "0")))
-    {
-        setcookie("id", "", time() - 3600*24*30*12, "/");
-        setcookie("hash", "", time() - 3600*24*30*12, "/");
-        print "Хм, что-то не получилось";
-    }
-    else
+   
     {
         print "Привет, ".$userdata['user_login'].". Всё работает!";
     }
