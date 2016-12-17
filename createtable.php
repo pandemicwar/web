20e68c7b4bbe16c87e745d4e982a118d40564774
pandemicwar <VasilevVS007@outlook.com>
2016-12-17 08:49:03 +0300

    Update createtable.php

--- createtable.php
+++ createtable.php
@@ -7,7 +7,7 @@ $db = "mysqlbase";
 try {
     $conn = new PDO("sqlsrv:server = tcp:serverforsqlbase.database.windows.net,1433; Database = mysqlbase", "vasilevvs007", "Spacedementia9");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $sql = "CREATE TABLE users(
+    $sql = "CREATE TABLE userss(
     id INT NOT NULL IDENTITY(1,1)
     PRIMARY KEY(id),
     login VARCHAR(30),
