b9249898ce388ca0b11bcc735fb6e561420dfe20
pandemicwar <VasilevVS007@outlook.com>
2016-12-17 08:47:16 +0300

    Update createtable.php

--- createtable.php
+++ createtable.php
@@ -11,7 +11,7 @@ try {
     user_id INT NOT NULL IDENTITY(1,1)
     PRIMARY KEY(id),
     login VARCHAR(30),
-    password VARCHAR(30)";
+    password VARCHAR(30))";
     $conn->query($sql);
 }
 catch (PDOException $e) {
