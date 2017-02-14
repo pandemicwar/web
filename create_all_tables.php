491d365b1f0db6b6d59dd7bb0b8eefed5448b60c
pandemicwar <VasilevVS007@outlook.com>
2017-02-14 22:58:30 +0300

    Update create_all_tables.php

--- create_all_tables.php
+++ create_all_tables.php
@@ -7,14 +7,13 @@ $db = "mysqlbase";
 try {
     $conn = new PDO("sqlsrv:server = tcp:serverforsqlbase.database.windows.net,1433; Database = mysqlbase", "vasilevvs007", "Spacedementia9");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $sql =   "CREATE TABLE firstfirst_tbl(
+    $sql =   "CREATE TABLE soft_projects_tbl(
     id_proj INT NOT NULL IDENTITY(1,1),
     name_proj VARCHAR(30),
     def_proj VARCHAR(30),
     spis_sotr INT,
-    PRIMARY KEY(id_proj),
-    FOREIGN KEY (spis_sotr) REFERENCES secondsecond_tbl (id_sotr)
-   
+    PRIMARY KEY(id_proj)
+       
    )";
     $conn->query($sql);
     }
@@ -24,17 +23,14 @@ catch (PDOException $e) {
 }
 echo "<h3>Table created.</h3>";
 
-try {
-    $conn = new PDO("sqlsrv:server = tcp:serverforsqlbase.database.windows.net,1433; Database = mysqlbase", "vasilevvs007", "Spacedementia9");
+ $conn = new PDO("sqlsrv:server = tcp:serverforsqlbase.database.windows.net,1433; Database = mysqlbase", "vasilevvs007", "Spacedementia9");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-     $sql =  "CREATE TABLE second_tbl(
+     $sql =  "CREATE TABLE soft_sotr_tbl(
      id_sotr INT NOT NULL IDENTITY(1,1),
      fio VARCHAR(30),
      doljnost VARCHAR(30),
      spisok_projectov_sotr INT,
-     PRIMARY KEY(id_sotr),
-     FOREIGN KEY (spisok_projectov_sotr) REFERENCES firstfirst_tbl (id_proj)
-       
+     PRIMARY KEY(id_sotr)   
      )";
     $conn->query($sql);
     }
@@ -46,14 +42,38 @@ echo "<h3>Table created.</h3>";
 try {
     $conn = new PDO("sqlsrv:server = tcp:serverforsqlbase.database.windows.net,1433; Database = mysqlbase", "vasilevvs007", "Spacedementia9");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-      $sql = "CREATE TABLE threeq_tbl(
+     $sql = "CREATE TABLE soft_zadachi_tbl(
+      soft_id INT NOT NULL,
+      soft_sotr_id INT NOT NULL,
+      
+      PRIMARY KEY (soft_id,soft_sotr_id),
+      FOREIGN KEY (soft_id) REFERENCES soft_projects_tbl (id_proj),
+      FOREIGN KEY (soft_sotr_id) REFERENCES soft_sotr_table (id_sotr)
+      )";
+  $conn->query($sql);
+}
+    
+    
+    
+    
+    
+    
+    
+    
+   
+
+
+
+try {
+    $conn = new PDO("sqlsrv:server = tcp:serverforsqlbase.database.windows.net,1433; Database = mysqlbase", "vasilevvs007", "Spacedementia9");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+      $sql = "CREATE TABLE soft_zadachi_tbl(
       id_zadachi INT NOT NULL IDENTITY(1,1),
       zadacha_kr_opis VARCHAR (100),
       opisanie_razv VARCHAR (500),
       ispolnitel INT,
       zatrati_chasi INT,
-      PRIMARY KEY (id_zadachi),
-      FOREIGN KEY (ispolnitel) REFERENCES secondsecond_tbl (id_sotr)
+      PRIMARY KEY (id_zadachi)
       )";
   $conn->query($sql);
 }
