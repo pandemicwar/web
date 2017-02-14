e593d627f537ce5876c52876d9bc43605af9e770
pandemicwar <VasilevVS007@outlook.com>
2017-02-14 21:05:56 +0300

    Update index.php

--- index.php
+++ index.php
@@ -7,13 +7,9 @@ $db = "mysqlbase";
 // Connect to database.
     $conn = new PDO("sqlsrv:server = tcp:serverforsqlbase.database.windows.net,1433; Database = mysqlbase", "vasilevvs007", "Spacedementia9");
     
- $sql = "SELECT * FROM `project_tbl`";
+ $sql = "SELECT * FROM `sotr_tbl`";
    $result = $conn->query($sql); 
    // В цикле перебираем все записи таблицы и выводим их
- while ($row = $result->fetch_assoc())
-   {
-       // Оператором echo выводим на экран поля таблицы name_blog и text_blog
-       echo 'Текст блога: '.$row['name_proj'];
-   }
+
    
 ?>
