cbf683fc84e0456a94be9609ac41d69f4c8b0422
pandemicwar <VasilevVS007@outlook.com>
2017-02-14 20:29:31 +0300

    Update index.php

--- index.php
+++ index.php
@@ -5,22 +5,11 @@ $user = "vasilevvs007";
 $pwd = "Spacedementia9";
 $db = "mysqlbase";
 // Connect to database.
-try {
     $conn = new PDO("sqlsrv:server = tcp:serverforsqlbase.database.windows.net,1433; Database = mysqlbase", "vasilevvs007", "Spacedementia9");
-    $conn->setAttribute
-( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
-}
-catch (PDOException $e) {
-    print("Error connecting to SQL Server.");
-    die(print_r($e));
-}
+    
  $sql = "SELECT * FROM `project_tbl`";
    $result = $conn->query($sql); 
    // В цикле перебираем все записи таблицы и выводим их
    while ($row = $result->fetch_assoc())
-   {
-       // Оператором echo выводим на экран поля таблицы name_blog и text_blog
-    
-       echo 'Текст блога: '.$row['def_project'];
-   }
+   
 ?>
