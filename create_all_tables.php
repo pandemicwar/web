09bbd0fda70fe5ca9713edf3f263fcb4c68f413a
pandemicwar <VasilevVS007@outlook.com>
2017-02-14 22:08:10 +0300

    Update create_all_tables.php

--- create_all_tables.php
+++ create_all_tables.php
@@ -7,7 +7,7 @@ $db = "mysqlbase";
 try {
     $conn = new PDO("sqlsrv:server = tcp:serverforsqlbase.database.windows.net,1433; Database = mysqlbase", "vasilevvs007", "Spacedementia9");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $sql =   "CREATE TABLE 1_tbl(
+    $sql =   "CREATE TABLE one_tbl(
     id_proj INT NOT NULL IDENTITY(1,1),
     name_proj VARCHAR(30),
     def_proj VARCHAR(30),
@@ -26,13 +26,13 @@ echo "<h3>Table created.</h3>";
 try {
     $conn = new PDO("sqlsrv:server = tcp:serverforsqlbase.database.windows.net,1433; Database = mysqlbase", "vasilevvs007", "Spacedementia9");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-     $sql =  "CREATE TABLE 2_tbl(
+     $sql =  "CREATE TABLE two_tbl(
      id_sotr INT NOT NULL IDENTITY(1,1),
      fio VARCHAR(30),
      doljnost VARCHAR(30),
      spisok_projectov_sotr INT,
      PRIMARY KEY(id_sotr),
-      FOREIGN KEY (spisok_projectov_sotr) REFERENCES 1_tbl (id_proj)    
+      FOREIGN KEY (spisok_projectov_sotr) REFERENCES one_tbl (id_proj)    
      )";
     $conn->query($sql);
     }
@@ -44,7 +44,7 @@ echo "<h3>Table created.</h3>";
 try {
     $conn = new PDO("sqlsrv:server = tcp:serverforsqlbase.database.windows.net,1433; Database = mysqlbase", "vasilevvs007", "Spacedementia9");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-      $sql = "CREATE TABLE 3_tbl(
+      $sql = "CREATE TABLE three_tbl(
       id_zadachi INT NOT NULL IDENTITY(1,1),
       zadacha_kr_opis VARCHAR (100),
       opisanie_razv VARCHAR (500),
