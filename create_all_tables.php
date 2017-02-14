13bc93522a6bf3561ae213c188e7a459442b46d1
pandemicwar <VasilevVS007@outlook.com>
2017-02-14 22:22:33 +0300

    Update create_all_tables.php

--- create_all_tables.php
+++ create_all_tables.php
@@ -7,13 +7,13 @@ $db = "mysqlbase";
 try {
     $conn = new PDO("sqlsrv:server = tcp:serverforsqlbase.database.windows.net,1433; Database = mysqlbase", "vasilevvs007", "Spacedementia9");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $sql =   "CREATE TABLE oneq_tbl(
+    $sql =   "CREATE TABLE first_tbl(
     id_proj INT NOT NULL IDENTITY(1,1),
     name_proj VARCHAR(30),
     def_proj VARCHAR(30),
     spis_sotr INT,
     PRIMARY KEY(id_proj),
-    FOREIGN KEY (spis_sotr) REFERENCES twoq_tbl (id_sotr)
+    FOREIGN KEY (spis_sotr) REFERENCES second_tbl (id_sotr)
    )";
     $conn->query($sql);
     }
@@ -26,12 +26,13 @@ echo "<h3>Table created.</h3>";
 try {
     $conn = new PDO("sqlsrv:server = tcp:serverforsqlbase.database.windows.net,1433; Database = mysqlbase", "vasilevvs007", "Spacedementia9");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-     $sql =  "CREATE TABLE twoq_tbl(
+     $sql =  "CREATE TABLE second_tbl(
      id_sotr INT NOT NULL IDENTITY(1,1),
      fio VARCHAR(30),
      doljnost VARCHAR(30),
-     spisok_projectov_sotr VARCHAR(30),
+     spisok_projectov_sotr INT,
      PRIMARY KEY(id_sotr),
+     FOREIGN KEY (spisok_projectov_sotr) REFERENCES first_tbl (id_sotr)
        
      )";
     $conn->query($sql);
@@ -51,7 +52,7 @@ try {
       ispolnitel INT,
       zatrati_chasi INT,
       PRIMARY KEY (id_zadachi),
-      FOREIGN KEY (ispolnitel) REFERENCES twoq_tbl (id_sotr)
+      FOREIGN KEY (ispolnitel) REFERENCES second_tbl (id_sotr)
       )";
   $conn->query($sql);
 }
