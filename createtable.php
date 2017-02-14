3e697a6d0dc3ae236fe1895c95d2e943efe5b98d
pandemicwar <VasilevVS007@outlook.com>
2017-02-14 19:13:04 +0300

    Update createtable.php

--- createtable.php
+++ createtable.php
@@ -7,12 +7,32 @@ $db = "mysqlbase";
 try {
     $conn = new PDO("sqlsrv:server = tcp:serverforsqlbase.database.windows.net,1433; Database = mysqlbase", "vasilevvs007", "Spacedementia9");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $sql = "CREATE TABLE registration_tbl(
-    id INT NOT NULL IDENTITY(1,1) 
-    PRIMARY KEY(id),
-    name VARCHAR(30),
-    email VARCHAR(30),
-    date DATE)";
+    $sql = "CREATE TABLE project_tbl(
+    id_proj INT NOT NULL IDENTITY(1,1) 
+    PRIMARY KEY(id_proj),
+    name_proj VARCHAR(30),
+    def_proj VARCHAR(30),
+    spis_sotr INT,
+    FOREIGN KEY (spis_sotr) REFERENCES sotr_tbl (id_sotr))";
+     $sql = "CREATE TABLE sotr_tbl(
+     id_sotr INT NOT NULL IDENTITY(1,1)
+     PRIMARY KEY(id_sotr),
+     fio VARCHAR(30),
+     doljnost VARCHAR(30),
+     spisok_projectov_sotr INT,
+      FOREIGN KEY (spisok_projectov_sotr) REFERENCES project_tbl (id_proj)     
+     )";
+      $sql = "CREATE TABLE zadacha_tbl(
+      id_zadachi INT INT NOT NULL IDENTITY(1,1)
+      PRIMARY KEY (id_zadachi),
+      zadacha_kr_opis VARCHAR (100),
+      opisanie_razv VARCHAR (500),
+      ispolnitel INT,
+      zatrati_chasi INT,
+      FOREIGN KEY (ispolnitel) REFERENCES sotr_tbl (id_sotr)
+      )";
+    
+    
     $conn->query($sql);
 }
 catch (PDOException $e) {
