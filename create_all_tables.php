5feb9e3b57699ba3cc83322a81645d3e2ac73222
pandemicwar <VasilevVS007@outlook.com>
2017-02-14 23:29:18 +0300

    Update create_all_tables.php

--- create_all_tables.php
+++ create_all_tables.php
@@ -7,7 +7,7 @@ $db = "mysqlbase";
 try {
     $conn = new PDO("sqlsrv:server = tcp:serverforsqlbase.database.windows.net,1433; Database = mysqlbase", "vasilevvs007", "Spacedementia9");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $sql =   "CREATE TABLE soft_projects_tbl(
+    $sql =   "CREATE TABLE softprojects_tbl(
     id_proj INT NOT NULL IDENTITY(1,1),
     name_proj VARCHAR(30),
     def_proj VARCHAR(30),
@@ -22,7 +22,7 @@ catch (PDOException $e) {
 }
 echo "<h3>Table created.</h3>";
 try {
-     $sql =  "CREATE TABLE soft_sotr_tbl(
+     $sql =  "CREATE TABLE sottr_tbl(
      id_sotr INT NOT NULL IDENTITY(1,1),
      fio VARCHAR(30),
      doljnost VARCHAR(30),
@@ -36,31 +36,16 @@ catch (PDOException $e) {
 }
 echo "<h3>Table created.</h3>";
 try{
-     $sql = "CREATE TABLE soft_zadachi_tbl(
-      soft_id INT NOT NULL,
-      softsotr_id INT NOT NULL,
-      
-      PRIMARY KEY (soft_id,softsotr_id),
-      FOREIGN KEY (soft_id) REFERENCES soft_projects_tbl (id_proj),
-      FOREIGN KEY (softsotr_id) REFERENCES soft_sotr_table (id_sotr)
-      
-      )";
-  $conn->query($sql); }
-catch (PDOException $e) {
-    print("Error connecting to SQL Server.");
-    die(print_r($e));
-}
-echo "<h3>Table created.</h3>";
-try{
     
-      $sql = "CREATE TABLE soft_zadachi_tbl(
+      $sql = "CREATE TABLE softzadachi_tbl(
       id_zadachi INT NOT NULL IDENTITY(1,1),
       zadacha_kr_opis VARCHAR (100),
       opisanie_razv VARCHAR (500),
       ispolnitel INT,
       zatrati_chasi INT,
-      PRIMARY KEY (id_zadachi)
-      )";
+      PRIMARY KEY (id_zadachi),
+      FOREIGN KEY (ispolnitel) REFERENCES sottr_tbl (id_sotr)
+      )" ENGINE=InnoDB;
   $conn->query($sql);}
 
 catch (PDOException $e) {
@@ -68,4 +53,21 @@ catch (PDOException $e) {
     die(print_r($e));
 }
 echo "<h3>Table created.</h3>";
+try{
+     $sql = "CREATE TABLE soft_zadachi_tbl(
+      soft_id INT NOT NULL,
+      softsotr_id INT NOT NULL,
+      
+      PRIMARY KEY (soft_id,softsotr_id),
+      FOREIGN KEY (soft_id) REFERENCES softprojects_tbl (id_proj),
+      FOREIGN KEY (softsotr_id) REFERENCES sottr_tbl (id_sotr)
+      
+      )" ENGINE=InnoDB;
+  $conn->query($sql); }
+catch (PDOException $e) {
+    print("Error connecting to SQL Server.");
+    die(print_r($e));
+}
+echo "<h3>Table created.</h3>";
+
 ?>
