acc6bf4e15ff602e16ec079db8daabc07e0816e7
pandemicwar <VasilevVS007@outlook.com>
2017-02-14 21:06:54 +0300

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ $db = "mysqlbase";
 // Connect to database.
     $conn = new PDO("sqlsrv:server = tcp:serverforsqlbase.database.windows.net,1433; Database = mysqlbase", "vasilevvs007", "Spacedementia9");
     
- $sql = "SELECT * FROM `sotr_tbl`";
+ $sql = "SELECT * FROM `zadacha_tbl`";
    $result = $conn->query($sql); 
    // В цикле перебираем все записи таблицы и выводим их
 
