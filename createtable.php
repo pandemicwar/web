f2e4a7dad851cb44c6378ce4724968de433c327c
pandemicwar <VasilevVS007@outlook.com>
2017-02-14 17:10:31 +0300

    Add files via upload

---
+++ createtable.php
@@ -0,0 +1,25 @@
+<?php
+// DB connection info
+$host = "localhost\sqlexpress";
+$user = "vasilevvs007";
+$pwd = "Spacedementia9";
+$db = "mysqlbase";
+try {
+    $conn = new PDO("sqlsrv:server = tcp:sqlbaseofrthesite.database.windows.net,1433; Database = mysqlbase", "vasilevvs007", "Spacedementia9");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $sql = "CREATE TABLE registration_tbl(
+    id INT NOT NULL IDENTITY(1,1) 
+    PRIMARY KEY(id),
+    name VARCHAR(30),
+    email VARCHAR(30),
+    date DATE)";
+    $conn->query($sql);
+}
+catch (PDOException $e) {
+    print("Error connecting to SQL Server.");
+    die(print_r($e));
+}
+
+echo "<h3>Table created.</h3>";
+?>
+
